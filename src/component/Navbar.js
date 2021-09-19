6bebff4bc395ecd986aa9f6b5741b97fe31104a2
Gaurav Kumar <55948253+Gauravkain@users.noreply.github.com>
2021-09-19 22:55:25 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,93 +1,7 @@
-// import React, { useState } from 'react';
-// import './navbar.css';
-// import { FaFacebookSquare, FaInstagramSquare, FaYoutubeSquare } from "react-icons/fa";
-// import { GiHamburgerMenu } from "react-icons/gi";
-
-
-// const Navbar = () => {
-//     const [showMediaIcons, setshowMediaIcons] = useState(false);
-//     return (
-//         <>
-//             <nav className="main-nav">
-//                 {/* 1st logo part */}
-//                 <div className="logo">
-//                     <h2>
-//                         <span>G</span>aurav
-//                         <span>K</span>ain
-//                     </h2>
-//                 </div>
-
-//                 {/* 2nd Menu part */}
-//                 <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
-//                     <ul>
-//                         <li>
-//                             <a href="#">Home</a>
-//                         </li>
-//                         <li>
-//                             <a href="#">About</a>
-//                         </li>
-//                         <li>
-//                             <a href="#">Service</a>
-//                         </li>
-//                         <li>
-//                             <a href="#">Contact</a>
-//                         </li>
-//                     </ul>
-//                 </div>
-
-//                 {/* 3rd social media links */}
-//                 <div className="social-media">
-//                     <ul className="social-media-desktop">
-//                         <li>
-//                             <a href="https://www.google.com" target="_google"><FaFacebookSquare className="facebook" /></a>
-//                         </li>
-//                         <li>
-//                             <a href="https://www.google.com" target="_google"><FaInstagramSquare className="instagram" /></a>
-//                         </li>
-//                         <li>
-//                             <a href="https://www.google.com" target="_google"><FaYoutubeSquare className="youtube" /></a>
-//                         </li>
-//                     </ul>
-
-//                     {/* hamburger menu start */}
-//                     <div className="hamburger-menu">
-//                         <a href="#" onClick={() => setshowMediaIcons(!showMediaIcons)}><GiHamburgerMenu /></a>
-//                     </div>
-//                 </div>
-//             </nav>
-
-//             {/* hero section  */}
-//             <section className="hero-section">
-//                 <p>Welcome to </p>
-//                 <h1>Gaurav kain blogs</h1>
-//             </section>
-//         </>
-//     );
-// };
-
-// export default Navbar;
-
-
-//NAVBAR
-
-
-//-------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from 'react';
 import './navbar.css';
 import { FaFacebookSquare, FaInstagramSquare, FaYoutubeSquare } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { NavLink } from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -107,16 +21,16 @@ const Navbar = () => {
                 <div className={showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}>
                     <ul>
                         <li>
-                            <NavLink to="/">Home</NavLink>
+                            <a href="#">Home</a>
                         </li>
                         <li>
-                            <NavLink to="/about">About</NavLink>
+                            <a href="#">About</a>
                         </li>
                         <li>
-                            <NavLink to="/service">Service</NavLink>
+                            <a href="#">Service</a>
                         </li>
                         <li>
-                            <NavLink to="/contact">Contact</NavLink>
+                            <a href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
@@ -125,13 +39,13 @@ const Navbar = () => {
                 <div className="social-media">
                     <ul className="social-media-desktop">
                         <li>
-                            <a href="https://www.facebook.com" target="_google"><FaFacebookSquare className="facebook"/></a>
+                            <a href="https://www.google.com" target="_google"><FaFacebookSquare className="facebook" /></a>
                         </li>
                         <li>
-                            <a href="https://www.instagram.com" target="_google"><FaInstagramSquare className="instagram"/></a>
+                            <a href="https://www.google.com" target="_google"><FaInstagramSquare className="instagram" /></a>
                         </li>
                         <li>
-                            <a href="https://www.youtube.com" target="_google"><FaYoutubeSquare className="youtube"/></a>
+                            <a href="https://www.google.com" target="_google"><FaYoutubeSquare className="youtube" /></a>
                         </li>
                     </ul>
 
@@ -143,12 +57,14 @@ const Navbar = () => {
             </nav>
 
             {/* hero section  */}
-            {/* <section className="hero-section">
+            <section className="hero-section">
                 <p>Welcome to </p>
                 <h1>Gaurav kain blogs</h1>
-            </section> */}
+            </section>
         </>
     );
 };
 
 export default Navbar;
+
+
