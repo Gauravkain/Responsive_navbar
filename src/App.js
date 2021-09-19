28d18f8a0f6b244395da966bec7416cf3cc5db3b
Gaurav Kumar <55948253+Gauravkain@users.noreply.github.com>
2021-09-19 22:54:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,96 +1,14 @@
-// NAVBAR
-
-// import React from 'react'
-// import Navbar from './component/Navbar';
-
-// const App = () => {
-//   return (
-//     <>
-//     <Navbar/>
-//     </>
-//   )
-// };
-
-// export default App;
-//NAVBAR
-//------------------------------------------------------------------------------------
-
-
 import React from 'react'
 import Navbar from './component/Navbar';
-import { Switch, Route } from "react-router-dom";
-
-
-const Home = () => {
-  return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Gaurav kain Home page</h1>
-      </section>
-    </>
-  );
-};
-
-const About = () => {
-  return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Gaurav kain About page</h1>
-      </section>
-    </>
-  );
-};
-
-const Service = () => {
-  return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Gaurav kain Service page</h1>
-      </section>
-    </>
-  );
-};
-
-const Contact = () => {
-  return (
-    <>
-      <Navbar />
-      <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Gaurav kain Contact page</h1>
-      </section>
-    </>
-  );
-};
-
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-
-      <Route exact path="/about">
-        <About />
-      </Route>
-
-      <Route exact path="/service">
-        <Service />
-      </Route>
-
-      <Route exact path="/contact">
-        <Contact />
-      </Route>
-
-    </Switch>
+    <>
+    <Navbar/>
+    </>
   )
 };
 
 export default App;
+
+
